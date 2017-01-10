94fbda04bebf9b026144644cc551dae35e1e0f50
Christoffer Sawicki <christoffer.sawicki@gmail.com>
2017-01-10 20:34:29 +0100

    Add shairport-sync

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,7 @@
 FROM resin/rpi-raspbian:jessie
 
+COPY provision.sh /tmp
+
+RUN /tmp/provision.sh && rm /tmp/provision.sh
+
 ENV INITSYSTEM on
---
+++ provision.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+URL="https://s3-eu-west-1.amazonaws.com/airplayer/shairport-sync_2.8.4-1~bpo8%2B1raspbian_armhf.deb"
+
+set -ex
+
+apt-get update
+
+apt-get install curl
+
+curl -o /tmp/shairport-sync.deb $URL
+
+dpkg -i /tmp/shairport-sync.deb || true
+
+apt-get -f install
+
+systemctl enable shairport-sync
+
+# Cleanup
+
+rm /tmp/shairport-sync.deb
+
+rm -rf /var/lib/apt/lists/*
