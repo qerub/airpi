c12770094f876d0a1e6466dd2faebaa8615e91f9
Christoffer Sawicki <christoffer.sawicki@gmail.com>
2017-01-16 20:56:24 +0100

    Fetch Shairport Sync configuration from CONFIGURATION_URL

--- entry.airpi.sh
+++ entry.airpi.sh
@@ -2,4 +2,11 @@
 
 set -e
 
+if [ -z "$CONFIGURATION_URL" ]; then
+  echo "Environment variable CONFIGURATION_URL not set" >&2
+  exit 1
+fi
+
+curl -o /etc/shairport-sync.conf "$CONFIGURATION_URL"
+
 exec /usr/bin/entry.sh "$@"
