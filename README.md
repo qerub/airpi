22919e0186fd577fe540b027a37bcc76d3b5c58a
Christoffer Sawicki <christoffer.sawicki@gmail.com>
2017-01-19 20:39:52 +0100

    Check integrity of shairport-sync.deb

--- README.md
+++ README.md
@@ -6,7 +6,3 @@
 2. Flash your Raspberry Pi with the "firmware" for that app.
 3. Define the app environment variable `CONFIGURATION_URL` to point at some Shairport Sync configuration.
 4. Push the code in this repository to the one Resin.io has created for your app.
-
-## Known limitations
-
-* External `shairport-sync-*.deb` fetched via cURL is not integrity checked.
--- provision.sh
+++ provision.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 URL="https://s3-eu-west-1.amazonaws.com/airplayer/shairport-sync_2.8.4-1~bpo8%2B1raspbian_armhf.deb"
+CHECKSUM="a57e56593da769222834e3f65d1fbea4f119f53b96d27a61148d245a65213ed7"
 
 set -ex
 
@@ -9,6 +10,8 @@ apt-get update
 apt-get install -y curl alsa-utils
 
 curl --fail --silent --show-error --location --output /tmp/shairport-sync.deb "$URL"
+
+echo "$CHECKSUM  /tmp/shairport-sync.deb" | sha256sum -c
 
 dpkg -i /tmp/shairport-sync.deb || true
 
